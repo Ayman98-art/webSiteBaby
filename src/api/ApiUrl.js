142bb8d27222f77df7ca80140083467c33adca89
Ayman98-art <ayman.goud99@gmail.com>
2023-06-15 00:40:32 +0300

    first commit

--- src/api/ApiUrl.js
+++ src/api/ApiUrl.js
@@ -1,2 +1,2 @@
-export const Basic_Url = 'http://localhost:4340/baby';
-export const urlProducts = "http://localhost:4340/baby";
+export const Basic_Url = 'https://babyjson.onrender.com/baby';
+export const urlProducts = "https://babyjson.onrender.com/baby";
